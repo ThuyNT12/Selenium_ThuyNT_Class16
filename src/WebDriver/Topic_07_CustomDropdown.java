cc185a08dae14557d0679b4fe73434133657e075
ThuyNT12 <tuylip.08@gmail.com>
2020-06-17 23:21:46 +0700

    Push Topic_07 TC_04 and TC_05

--- src/WebDriver/Topic_07_CustomDropdown.java
+++ src/WebDriver/Topic_07_CustomDropdown.java
@@ -5,8 +5,11 @@ package WebDriver;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.security.auth.kerberos.KerberosKey;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -57,11 +60,36 @@ public class Topic_07_CustomDropdown {
 			selectItemDropDown("//ejs-dropdownlist[@id='games']//span//span[contains(@class,'e-search-icon')]", "//ul[@id='games_options']//li", "Football");
 			Assert.assertEquals(getHiddenItem("select[id='games_hidden'] option"), "Football");
 			
-			selectItemDropDown("//ejs-dropdownlist[@id='games']//span//span[contains(@class,'e-search-icon')]", "//ul[@id='games_options']//li", "Golf");
-			Assert.assertEquals(getHiddenItem("select[id='games_hidden'] option"), "Golf");
+			selectItemDropDown("//ejs-dropdownlist[@id='games']//span//span[contains(@class,'e-search-icon')]", "//div[@role='listbox']//div//span", "Justen Kitsune");
+			Assert.assertEquals(getHiddenItem("select[id='games_hidden'] option"), "Justen Kitsune");
 		}
 
-		
+		@Test
+		public void TC_03_ReactJS() {
+			driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+			selectItemDropDown("//i[@class='dropdown icon']", "//span[@class='text']", "Jenny Hess");
+			Assert.assertTrue(driver.findElement(By.xpath("//div[@role='listbox']/div[@class='text' and text()='Jenny Hess']")).isDisplayed());
+			
+			selectItemDropDown("//i[@class='dropdown icon']", "//span[@class='text']", "Christian");
+			Assert.assertTrue(driver.findElement(By.xpath("//div[@role='listbox']/div[@class='text' and text()='Christian']")).isDisplayed());	
+		}
+		@Test
+		public void TC_04_Editable() {
+			driver.get("http://indrimuska.github.io/jquery-editable-select/");
+			sendKeysToEditDropdown("//div[@id='default-place']//input","BMW");
+			Assert.assertEquals(getHiddenItem("#default-place li.es-visible"), "BMW");
+			sendKeysToEditDropdown("//div[@id='default-place']//input","Audi");
+			Assert.assertEquals(getHiddenItem("#default-place li.es-visible"), "Audi");
+			sendKeysToEditDropdown("//div[@id='default-place']//input","Ford");
+			Assert.assertEquals(getHiddenItem("#default-place li.es-visible"), "Ford");
+		}
+		@Test
+		public void TC_05_Advance() {
+			driver.get("http://multiple-select.wenzhixin.net.cn/examples#basic.html");
+			driver.switchTo().frame(driver.findElement(By.tagName("iframe")));
+			
+			
+		}
 		@AfterClass
 		public void afterClass() {
 			driver.quit();
@@ -91,15 +119,46 @@ public class Topic_07_CustomDropdown {
 				}
 				
 			}
-			
-			
-			
-			
 		}
 		
+		
 		public String getHiddenItem(String ccsLocator) {
-			return (String) JsExcutor.executeScript("return document.querySelector(\"" + ccsLocator + "\").text");
-			
+			return (String) JsExcutor.executeScript("return document.querySelector(\"" + ccsLocator + "\").textContent");
+		}
+		
+		public void sendKeysToEditDropdown(String locator, String value) {
+			driver.findElement(By.xpath(locator)).clear();
+			driver.findElement(By.xpath(locator)).sendKeys(value);
+			sleepInSecond(1);
+			driver.findElement(By.xpath(locator)).sendKeys(Keys.TAB);
+			sleepInSecond(1);
+		}
+		public void selectMultipleDropdown(String parentXpath, String allItemXpath, String[] expectedValueItem) {
+			//Click vao dropdown cho no xo het gia tri
+			WebElement parentDropdown = driver.findElement(By.xpath(parentXpath));
+			explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(allItemXpath)));
+			List<WebElement> allItems = driver.findElements(By.xpath(allItemXpath));
+			//Duyet qua cac phan tu cho den khi thoa man dieu kien
+			for(WebElement chilElement: allItems) {
+				for(String item:expectedValueItem) {
+					if(chilElement.getText().equals(item)) {
+						JsExcutor.executeScript("arguments[0]. scrollIntoView(true);", chilElement);
+						sleepInSecond(2);
+						//click vao item can chon
+						JsExcutor.executeScript("arguments[0].click();", chilElement);
+						sleepInSecond(2);
+						List<WebElement> itemSelected = driver.findElements(By.xpath("//li[@class='selected']//input"));
+						if(expectedValueItem.length==itemSelected.size()) {
+							break;
+						}
+					}
+
+			}
+			}
+		}
+		public boolean checkItemSelected (String[] itemSelectedText) {
+			List<WebElement> itemSelected = driver.findElements(By.xpath("//li[@class='selected']//input\""));
+			return false;
 			
 		}
 		public void sleepInSecond(long time){
