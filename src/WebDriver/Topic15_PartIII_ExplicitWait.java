d320d45e673e02a82e8896fa58eb05b5a6d476d7
ThuyNT <tuylip.08@gmail.com>
2020-08-12 16:23:16 +0700

    Topic15_partIII_TC04_uploadfiles

--- src/WebDriver/Topic15_PartIII_ExplicitWait.java
+++ src/WebDriver/Topic15_PartIII_ExplicitWait.java
@@ -1,7 +1,11 @@
 package WebDriver;
 
+import java.util.Set;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,12 +17,22 @@ import org.testng.annotations.Test;
 public class Topic15_PartIII_ExplicitWait {
 	WebDriver driver;
 	WebDriverWait ExplicitWait;
-	//check push
+	JavascriptExecutor jsExecutor;
+	WebElement element;
+	String source_Folder = System.getProperty("user.dir");
+	String image_Name_01 = "HoaHong.jpg";
+	String image_Name_02 = "HoaSung.jpg";
+	String image_Name_03 = "HuongDuong.jpg";
+	String image_01_path = source_Folder  + "\\Upload_File\\" + image_Name_01;
+	String image_02_path = source_Folder  + "\\Upload_File\\" + image_Name_02;
+	String image_03_path = source_Folder  + "\\Upload_File\\" + image_Name_03;
+	
 		@BeforeClass
 		public void beforeClass() {
 			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/BrowserDriver/geckodriver.exe");
 			driver = new FirefoxDriver();
-			ExplicitWait = new WebDriverWait(driver, 10);
+			ExplicitWait = new WebDriverWait(driver, 30);
+			jsExecutor = (JavascriptExecutor) driver;
 			driver.manage().window().maximize();
 			
 		}
@@ -60,8 +74,71 @@ public class Topic15_PartIII_ExplicitWait {
 			//Step 7: validate selected date
 			Assert.assertEquals(driver.findElement(By.id("ctl00_ContentPlaceholder1_Label1")).getText(), "Tuesday, August 11, 2020");
 			
+		}
+		@Test
+		public void TC04_uploadFiles() {
+			driver.get("https://gofile.io/?t=uploadFiles");
+			//Wait Update button visible
+			ExplicitWait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("btnChooseFiles")));
+			//Step 2: Upload file
+			String parentID = driver.getWindowHandle();
+			WebElement UploadFile = driver.findElement(By.xpath("//input[@type='file']"));
+			UploadFile.sendKeys(image_01_path + "\n" + image_02_path + "\n" + image_03_path);
+
+			//Wait Upload button visible and click
+			scrollToElement("//button[@id='btnUpload']");
+			ExplicitWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='btnUpload']"))).click();
+			//Click upload button 
+			//driver.findElement(By.cssSelector("button#btnUpload")).click();
+		
+			//Wait the process bar invisible
+			ExplicitWait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("progressBarData")));
+			//Step 4: Click OK button
+			ExplicitWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Ok']"))).click();
 			
 			
+			//Click on link upload file
+			ExplicitWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@id='link']"))).click();
+			//driver.findElement(By.xpath("//a[@id='link']")).click();
+			//Switch to link
+			witchWindowByID(parentID);
+			ExplicitWait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//td[text()='" + image_Name_01 + "']/following-sibling::td[@class]//i[contains(@class,'download')]")));
+			//Verify icon download display
+			//driver.findElement(By.xpath("//td[text()='HoaHong.jpg']/following-sibling::td[@class]//i[contains(@class,'download')]"));
+			Assert.assertTrue(driver.findElement(By.xpath("//td[text()='" + image_Name_01 + "']/following-sibling::td[@class]//i[contains(@class,'download')]")).isDisplayed());
+			Assert.assertTrue(driver.findElement(By.xpath("//td[text()='" + image_Name_02 + "']/following-sibling::td[@class]//i[contains(@class,'download')]")).isDisplayed());
+			Assert.assertTrue(driver.findElement(By.xpath("//td[text()='" + image_Name_03 + "']/following-sibling::td[@class]//i[contains(@class,'download')]")).isDisplayed());
+		
+			//Verify icon Play display
+			Assert.assertTrue(driver.findElement(By.xpath("//td[text()='" + image_Name_01 + "']/following-sibling::td[@class]//i[contains(@class,'play')]")).isDisplayed());
+			Assert.assertTrue(driver.findElement(By.xpath("//td[text()='" + image_Name_02 + "']/following-sibling::td[@class]//i[contains(@class,'play')]")).isDisplayed());
+			Assert.assertTrue(driver.findElement(By.xpath("//td[text()='" + image_Name_03 + "']/following-sibling::td[@class]//i[contains(@class,'play')]")).isDisplayed());
+		}
+		public void witchWindowByID(String parentID) {
+			Set<String> allWindows = driver.getWindowHandles();
+			for (String runWindow: allWindows) {
+				if(!runWindow.equals(parentID)) {
+					driver.switchTo().window(runWindow);
+					break;
+				}
+			}
+					
+		}
+		public boolean isImageDisplayed(String xpathLocator) {
+			
+			Boolean imagePresence = (Boolean) ((JavascriptExecutor) driver)
+					.executeScript("return arguments[0].complete && typeof arguments[0].naturalWidth " + 
+			"!= \"undefined\" && arguments[0].naturalWidth > 0", 
+			driver.findElement(By.xpath(xpathLocator)));
+			return imagePresence;
+		}
+		public void scrollToElement(String locator) {
+			 element = driver.findElement(By.xpath(locator));
+			jsExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
+		}
+		public void clickToElementByJS(String locator) {
+			element = driver.findElement(By.xpath(locator));
+			jsExecutor.executeScript("arguments[0].click();", element);
 		}
 		@AfterClass
 		public void afterClass() {
